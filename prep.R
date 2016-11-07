ebd3f5d7eac77e107fd6ff23f21731b8cee4ea12
Stuart Allen <stuart@stuartallen.com.au>
2016-11-08 07:41:32 +1100

    Tidy and remove redundant code from prep

--- prep.R
+++ prep.R
@@ -2,7 +2,6 @@ library(dplyr)
 library(raster)
 library(rgeos)
 library(maptools)
-#library(ggplot2)
 library(rgdal)
 library(gdalUtils)
 
@@ -13,8 +12,10 @@ rm(list = ls())
 source("R/globals.R")
 source("R/range.R")
 
-### get Myrtle Rust rasters
+############################################################################
+### prepare Myrtle Rust rasters
 
+# re-project to equal area projection, use nearest neighbour to retain 1 values
 mrCurrent <- raster("data/myrtle_rust/raster/PA_Current.gri") %>%
   projectRaster(crs = aea, method = "ngb")
 
@@ -25,28 +26,29 @@ mrFuture <- raster("data/myrtle_rust/raster/PA_Future.gri") %>%
 mrCurrent[mrCurrent == 0] <- NA
 mrFuture[mrFuture == 0] <- NA
 
-# write output rasters
+# write output MR rasters
 writeRaster(mrCurrent, "output/myrtle_rust/mr_current.tif", overwrite = TRUE)
 writeRaster(mrFuture, "output/myrtle_rust/mr_future.tif", overwrite = TRUE)
 
-
-### get occurrence data
+############################################################################
+### prepare occurrence data
 
 data <- read.csv("data/occurrence/New_Myrt_Aust_Complete_270916.csv", stringsAsFactors = FALSE) %>%
   setNames(c("id", "species", "latitude", "longitude")) %>%
   filter(!is.na(longitude) & !is.na(latitude))
 write.csv(data, "output/occurrence/occurrence.csv", row.names = FALSE)
 
+############################################################################
+### prepare EOO MCPs for each species
+
 # get Australia boundary
 aus <- readOGR("data/gis/australia/SHAPEFILE.shp", layer = "SHAPEFILE") %>%
   spTransform(aea)
 
-
-### store taxon spatial data
-
 # get list of taxa
 taxa <- unique(data$species) %>% sort
 
+# create output vector
 mcp <- vector("list")
 
 for (taxon in taxa) {
@@ -75,7 +77,7 @@ for (taxon in taxa) {
   # create data frame of cropped MCP vertices
   mcpDF <- polygonToDataFrame(mcpCrop)  
   
-  # write to list
+  # write to output vector
   mcp[[taxon]]$mcp_full <- mcpFull
   mcp[[taxon]]$num_vertices <- numVertices
   mcp[[taxon]]$mcp_cropped <- mcpCrop
@@ -83,45 +85,50 @@ for (taxon in taxa) {
 
 }
 
+# write mcp data to file
 saveRDS(mcp, "output/mcp.rds")
 
-###
+##############################################################################
+### prepare species rasters for EOO (cropped MCP) and intersection of EOO & MR
 
+# get mcp data
 mcp <- readRDS("output/mcp.rds")
 
+# get MR rasters
 mrCurrent <- raster("output/myrtle_rust/mr_current.tif")
 mrFuture <- raster("output/myrtle_rust/mr_future.tif")
 
-tempShp <- "temp/temp.shp"
-
-for (taxon in taxa[20]) {
+for (taxon in taxa) {
   message(taxon)
   
-  # generate base raster for MCP and 
-  # only need to have raster extent of MCP polygon, no need to have full Aus extent  ===== OBSOLETE???
-  e <- alignExtent(extent(mcp[[taxon]]$mcp_cropped), mrCurrent)
-  baseRaster <- raster(ext = e, resolution = res(mrCurrent), crs = aea)
+  # generate base raster for MCP; only need to have raster extent of MCP polygon, no need to have full Aus extent
+  baseRaster <- mcp[[taxon]]$mcp_cropped %>%
+    extent %>%
+    alignExtent(mrCurrent) %>%
+    raster(ext = ., resolution = res(mrCurrent), crs = aea)
   
+  # write base raster to temp file; this is used as base for output of rgdal_rasterize
+  writeRaster(baseRaster, "temp/temp.tif", overwrite = TRUE)  
+  
+  # write MCP polygon to temp file
   mcp[[taxon]]$mcp_cropped %>%
     as("SpatialPolygonsDataFrame") %>%
-    writeOGR(dirname(tempShp), gsub("[.]shp", "", basename(tempShp)), driver = "ESRI Shapefile", overwrite = TRUE)
-  
-  writeRaster(baseRaster, "temp/temp.tif", overwrite = TRUE)
-  
-  mcpRast <- gdal_rasterize(b = 1, burn = 1, l = "temp", src_datasource = tempShp, dst_filename = "temp/temp.tif", output_Raster = TRUE) %>%
+    writeOGR("temp", "temp", driver = "ESRI Shapefile", overwrite = TRUE)
+    
+  # rasterize MCP
+  mcpRast <- gdal_rasterize(b = 1, burn = 1, l = "temp", src_datasource = "temp/temp.shp", dst_filename = "temp/temp.tif", output_Raster = TRUE) %>%
     raster(layer = 1)
-  
-  # method using MUCH slower raster::rasterize()
-  #mcpRast <- rasterize(mcp[[taxon]]$mcp_cropped, baseRaster)
   
   # write cropped raster to file
   writeRaster(mcpRast, paste0("output/mcp/", gsub(" ", "_", taxon), ".tif"), overwrite = TRUE)
   
-  # compute raster of overlap between MCP and MR
-  x <- crop(mrCurrent, mcpRast)
-  out <- raster::mask(mcpRast, x, maskvalue = NA)
-
-  writeRaster(out, paste0("output/mcp_mr/", gsub(" ", "_", taxon), ".tif"), overwrite = TRUE)
+  # compute raster of overlap between MCP and MR; need to first crop MR raster to MCP raster extent
+  mrOverlap <- mrCurrent %>%
+    crop(mcpRast) %>%
+    raster::mask(mcpRast, ., maskvalue = NA)
+  
+  # write 
+  writeRaster(mrOverlap, paste0("output/mcp_mr/", gsub(" ", "_", taxon), ".tif"), overwrite = TRUE)
 }
 
 
